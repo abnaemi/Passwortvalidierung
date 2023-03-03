b09066e18fed047d21cacf54a7c8fa98533f51dc
abnae <ab.naemi@gmail.com>
2023-03-03 10:13:38 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+
+
+//    Scanner input = new Scanner (System.in);
+//        System.out.println("Gib dein Passwort ein");
+//
+//                String passwort = input.nextLine();
+//
+//                System.out.println(passwort.length() );
+//
+//                if (passwort.matches(".*\\d.*"))
+//                {System.out.println("contains a number");}
+//                else {
+//                System.out.println("no number");
+//                }
+//
+//                if (passwort.contains("123456" || "passwort")) {
+//                return "weak passwort"
+//                }  else
+//                return "good passwort"
